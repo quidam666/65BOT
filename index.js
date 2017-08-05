ab37800b97c7ea1f9fb67554465c9d1db6bef2f0
HsiangMinYu <hsiangmin16@gmail.com>
2017-08-05 13:32:36 +0800

    welfare

--- index.js
+++ index.js
@@ -208,8 +208,7 @@ function showActivities(event, pgClient, userProfile, zone) {
 }
 
 function findWelfares(event) {
-    console.log("[Index] (findWelfares) " + mIdentity + " / " +
-        " / " + mNeedCare + " / " + mNeedAssisive)
+    console.log("[Index] (findWelfares) " + mCity + "/" + mIdentity + " / " + mNeedCare + " / " + mNeedAssisive)
 
     if (mIdentity === undefined) {
         hsBOT.askIdentify(event)
