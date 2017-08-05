cc0638d157c47396e66aaf6c7f7f1cd561187a70
HsiangMinYu <hsiangmin16@gmail.com>
2017-08-05 10:49:45 +0800

    show activities

--- hsbot.js
+++ hsbot.js
@@ -2,8 +2,8 @@ const util = require('util')
 
 module.exports = {
 
-    showUserText: function(event,message){
-        event.reply({ type: 'text', text: message})
+    repeatUserText: function (event, message) {
+        event.reply({ type: 'text', text: message })
     },
 
     showWelcomeText: function (event, userProfile) {
@@ -375,7 +375,8 @@ module.exports = {
         var columns
         module.exports.getActivityColumns(activities).then(function (result) {
             columns = result
-            console.log('(showActivitiesInCarousel)' + result)
+            console.log('(showActivitiesInCarousel)' + result + userProfile.displayName + " / "
+         + userProfile.name)
             // console.log("(Carousel) getActivityColumns " + util.inspect(columns, false, null))
 
             event.reply({
